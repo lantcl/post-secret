2161c17502f05b3d9db52188ac4467ffa7859179
Cleo Lant <lantc@shernet.sheridancollege.ca>
2019-03-29 11:56:43 -0400

    built pages

--- scripts/control.js
+++ scripts/control.js
@@ -12,15 +12,15 @@ var app = function(app){  //module pattern
 		// });
 
 		//v.dial.on("change" functionetc...) not chainable 
-		v.dial.change(()=>{ //chainable
-			m.data[0] = v.dial.currentValue;
-			m.updateData();
-		});
+		// v.dial.change(()=>{ //chainable
+		// 	m.data[0] = v.dial.currentValue;
+		// 	m.updateData();
+		// });
 
-		v.slider.change(()=>{ 
-			m.data[1] = v.slider.currentValue;   //basically this keeps the same values for the slider and dial if you refresh the page
-			m.updateData();
-		});
+		// v.slider.change(()=>{ 
+		// 	m.data[1] = v.slider.currentValue;   //basically this keeps the same values for the slider and dial if you refresh the page
+		// 	m.updateData();
+		// });
 		
 		const hs = new HotSpots([  //use loops whenn you need repeated stuff
 			{
@@ -29,8 +29,8 @@ var app = function(app){  //module pattern
 				//rect:[0,0,100,100], 
 				call:()=>{zog("clicking on hotspot")}
 			},
-			{page: v.page2,rect: v.page2.logo,call:()=>{zog("clicking on hotspot 2")}},
-			{page: v.page1, rect: v.page1.tabs.buttons[1], call:()=>{v.pages.go(v.page2, "down");}},
+			{page: v.page2, rect: v.page2.logo,call:()=>{zog("clicking on hotspot 2")}},
+			{page: v.page1, rect: v.page1.button, call:()=>{v.pages.go(v.page2, "down");}},
 			{page: v.page2, rect: v.page2.tabs.buttons[0], call:()=>{v.pages.go(v.page1, "up");}
 			}
 		]);
--- scripts/view.js
+++ scripts/view.js
@@ -1,7 +1,7 @@
 var app = function(app){  //module pattern
 	app.makeView = function(m, stage){
 		const v = {};
-
+		
 		const manager = v.manager = new Manager();
 
 		var stageW = stage.width;
@@ -14,78 +14,119 @@ var app = function(app){  //module pattern
 					width: 160,
 					spacing: 20
 				},
-				Button:{
-					width: 60,
-					height:60,
+			Button:{
+					width: 40,
+					height:40,
 					shadowBlur: -1,
 					label:"",
-					corner:30,
-					backgroundColor: grey,
-					rollBackgroundColor: black
+					corner:20,
+					font: "courier",
+					backgroundColor: "#47163A",
+					rollBackgroundColor: "#330033"
 				}
 			}
 		}
 		
-	
+	 
 		
 		////////////PAGE 1///////
 		const page1 = v.page1 = new Container(stageW, stageH);
 		
 		let header = new Container().addTo(page1);
-		v.page1.logo =  new Label(m.title).addTo(header);
+		// v.page1.logo =  new Label(m.title).addTo(header);
+		v.page1.logo = frame.asset("logo.png").addTo(header);
+		
 		let content = new Container(300,300).addTo(page1);
 		
-		v.dial = new Dial().center(content);
-		v.dial.currentValue = m.data[0];
+		let startButton = v.page1.button = new Button({
+			label: "enter",
+			size: 10,
+			corner: 2,
+			width: 150,
+			height: 50
+		}).center(content);
 
-		let footer = v.page1.tabs = new Tabs({
-			tabs:[new Button({
-				icon: pizzazz.makeIcon("home", "white").alp(.7),
-				rollIcon: pizzazz.makeIcon("home", "white")
-			}), 
-			new Button({
-				icon: pizzazz.makeIcon("settings", "white").alp(.7),
-				rollIcon: pizzazz.makeIcon("settings", "white")
-			})
-			]
-		}).addTo(page1);
-		//footer.buttons[0].setIcon("icon", pizzazz.makeIcon("settings", "white"))
 
 		const layout1 = new Layout(page1, [
-			{object: header, marginTop:5,  maxWidth: 90, backgroundColor:green},
-			{object: content, marginTop:2, backgroundColor:green},
-			{object: footer, marginTop:2, maxWidth: 90}
-			], 2, yellow, true, null, stage);
+			{object: header, marginTop:10,  maxWidth: 90},
+			{object: content, marginTop:2}
+
+			], 2, "#3E303B", true, null, stage);
 
 		manager.add(layout1);
 
 		////////////PAGE 2///////
 		const page2 = v.page2 = new Container(stageW, stageH);
 
-		header = new Container().addTo(page2);
-		v.page2.logo = new Label(m.title).addTo(header);
-		content = new Container(300,300).addTo(page2);
-		v.slider = new Slider().sca(.5).center(content);
-		v.slider.currentValue = m.data[1];
+		let header2 = new Container().addTo(page2);
+		let logo2 = v.page2.logo = frame.asset("page2-header.png").addTo(header2);
+		let content2 = new Container(300,300).addTo(page2);
 		
-		// footer = v.page2.tabs = new Tabs({tabs:[1,2]}).addTo(page2);
-		footer = v.page2.tabs = footer.clone().addTo(page2);
+		//var bg = frame.asset("bg-test.jpg").addTo(content);
 		
-		layout2 = new Layout(page2, [
-			{object: header, marginTop:5,  maxWidth: 90, backgroundColor:blue},
-			{object: content, marginTop:2, backgroundColor:blue},
-			{object: footer, marginTop:2,  maxWidth: 90}
-			], 2, yellow, true, null, stage);
+		let footer2 = v.page2.tabs = new Tabs({
+			tabs:[new Button({
+				icon: pizzazz.makeIcon("home", "white").sca(.5),
+				rollIcon: pizzazz.makeIcon("home", "white").sca(.6)
+			}), 
+			new Button({
+				icon: pizzazz.makeIcon("plus", "white").sca(.5),
+				rollIcon: pizzazz.makeIcon("plus", "white").sca(.6)
+			})
+			]
+		}).addTo(page2);
+		
+		const layout2 = new Layout(page2, [
+			{object: header2, marginTop:5,  maxWidth: 90,},
+			{object: content2, marginTop:2},
+			{object: footer2, marginTop:2,  maxWidth: 90}
+			], 2, "#3E303B", true, null, stage);
 
 
 		manager.add(layout2);
 		
+
+		////////////PAGE 3///////
+		const page3 = v.page3 = new Container(stageW, stageH);
+
+		let header3 = new Container().addTo(page3);
+		v.page3.logo = new Label({
+			text: "add a new secret",
+			font: "courier",
+			color: white
+		}).addTo(header3);
+		let content3 = new Container(300,300).addTo(page3);
+		
+		var bg = frame.asset("bg-test.jpg").centerReg(page3).sca(.2);
+		
+		let footer3 = v.page3.tabs = new Tabs({
+			tabs:[new Button({
+				icon: pizzazz.makeIcon("home", "white").sca(.5),
+				rollIcon: pizzazz.makeIcon("home", "white").sca(.6)
+			}), 
+			new Button({
+				icon: pizzazz.makeIcon("close", "white").sca(.5),
+				rollIcon: pizzazz.makeIcon("close", "white").sca(.6)
+			})
+			]
+		}).addTo(content3);
+		
+		const layout3 = new Layout(page3, [
+			{object: header3, marginTop:5,  maxWidth: 90,},
+			{object: content3, marginTop:2},
+			{object: footer3, marginTop:2,  maxWidth: 90}
+			], 2, "#3E303B", true, null, stage);
+
+
+		manager.add(layout3);
+		
 		v.pages = new Pages([
-			{page:page1, swipe:[null, null, page2, page2]},
-			{page:page2, swipe:[null, null, page1, page1]}
+			{page:page1, swipe:[null, null, page1, page2]},
+			{page:page2, swipe:[null, null, page1, page3]},
+			{page:page3, swipe:[null, null, page2, page3]},
 			], "slide", 500).addTo();
 		
-		manager.add([v.pages]);
+		manager.add(v.pages);
 	
 	return v;
 	}
